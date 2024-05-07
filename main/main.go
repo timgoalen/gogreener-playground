f305699613966ec94bbf6e2c8214b7d4166db4b4
Tim Goalen <timgoalen@hotmail.com>
2024-05-07 12:47:58 +0000

    feat: hello world example

--- main/main.go
+++ main/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"context"
 	"github.com/thejimmyg/greener"
+	"log"
 )
 
 func main() {
-
+	app := greener.NewDefaultApp(
+		greener.NewDefaultServeConfigProviderFromEnvironment(),
+		greener.NewDefaultLogger(log.Printf),
+		greener.NewDefaultEmptyPageProvider([]greener.Injector{}),
+	)
+	app.HandleWithServices("/", func(s greener.Services) {
+		s.W().Write([]byte(app.Page("Hello", greener.Text("Hello <>!"))))
+	})
+	app.Serve(context.Background())
 }
